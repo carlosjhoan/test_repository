4327b2974ce630bba2a0abae51b090370a74987c
carlosjhoan <carlosjhoanaguilar@gmail.com>
2024-01-31 21:13:11 -0500

    creando el archivo save_data

--- js/conv-divisas.js
+++ js/conv-divisas.js
@@ -1,10 +1,1 @@
-document.getElementById("guardar").addEventListener("click", () => {
-    console.log("hola")
-})
-function save_data () {
-   fetch("../data/data.csv")
-   .then(data => data.text())
-   .then(dat => console.log(dat))
-   .catch("hay un error")
-}
 
